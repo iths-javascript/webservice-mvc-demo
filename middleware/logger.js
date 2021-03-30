59794e1ca4e1e7d805bef602d38f036503f9379a
David Lundholm <david.lundholm@zocom.se>
2021-03-30 08:53:31 +0200

    Fix syntax error

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,7 @@ const MY_CONSTANT = 1000
 const MY_CONSTANT_CONSTANT = MY_CONSTANT + 100
 
 const Logger = (req,res,next) => {
-  console.log(`${new Date(} - ${req.method} - ${req.path}`)
+  console.log(`${new Date()} - ${req.method} - ${req.path}`)
   next()
 }
 
